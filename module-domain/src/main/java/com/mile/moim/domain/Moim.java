612818a321a5007cee64bb591a8b7e6df52101e6
sohyundoh <lyny.doh@gmail.com>
2024-01-04 14:33:08 +0900

    #11 [feat] DDL Exception 해결

--- module-domain/src/main/java/com/mile/moim/domain/Moim.java
+++ module-domain/src/main/java/com/mile/moim/domain/Moim.java
@@ -6,12 +6,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "Moim")
 public class Moim {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
     @ManyToOne
     private User owner;
     private String name;
