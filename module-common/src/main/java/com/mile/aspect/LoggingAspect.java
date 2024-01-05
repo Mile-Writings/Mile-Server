10a6d6dab0315abd834fa7caee1033af310fc55f
sohyundoh <lyny.doh@gmail.com>
2024-01-05 23:01:34 +0900

    #15 [feat] 로깅 로직 구현

---
+++ module-common/src/main/java/com/mile/aspect/LoggingAspect.java
@@ -0,0 +1,61 @@
+package com.mile.aspect;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Pointcut("execution(* com.mile.handler.GlobalExceptionHandler.handleException*(..))")
+    public void controllerErrorLevelExecute() {
+    }
+
+
+    @Around("com.mile.aspect.LoggingAspect.controllerErrorLevelExecute()")
+    public Object requestErrorLevelLogging(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        final ContentCachingRequestWrapper cachingRequest = (ContentCachingRequestWrapper) request;
+        long startAt = System.currentTimeMillis();
+        Object returnValue = proceedingJoinPoint.proceed(proceedingJoinPoint.getArgs());
+        long endAt = System.currentTimeMillis();
+
+        log.error("====> Request: {} {} ({}ms)\n *Header = {}", request.getMethod(), request.getRequestURL(), endAt - startAt, getHeaders(request));
+        if ("POST".equalsIgnoreCase(request.getMethod())) {
+            log.error("====> Body: {}", objectMapper.readTree(cachingRequest.getContentAsByteArray()));
+        }
+        if (returnValue != null) {
+            log.error("====> Response: {}", returnValue);
+        }
+        log.error("================================================END===============================================");
+        return returnValue;
+    }
+
+    private Map<String, Object> getHeaders(HttpServletRequest request) {
+        Map<String, Object> headerMap = new HashMap<>();
+
+        Enumeration<String> headerArray = request.getHeaderNames();
+        while (headerArray.hasMoreElements()) {
+            String headerName = headerArray.nextElement();
+            headerMap.put(headerName, request.getHeader(headerName));
+        }
+        return headerMap;
+    }
+}
--- module-common/src/main/java/com/mile/handler/GlobalExceptionHandler.java
+++ module-common/src/main/java/com/mile/handler/GlobalExceptionHandler.java
@@ -1,17 +1,23 @@
-package com.mile.common.handler;
+package com.mile.handler;
 
-import com.mile.common.dto.ErrorResponse;
+import com.mile.dto.ErrorResponse;
 import com.mile.exception.message.ErrorMessage;
 import com.mile.exception.model.BadRequestException;
 import com.mile.exception.model.MileException;
 import com.mile.exception.model.NotFoundException;
 import com.mile.exception.model.UnauthorizedException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.IOException;
+
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -34,6 +40,15 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleNotFoundException(final NotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.of(e.getErrorMessage()));
     }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    protected ErrorResponse handleException(final Exception error, final HttpServletRequest request) {
+        log.error("================================================NEW===============================================");
+        log.error(error.getMessage(), error);
+        return ErrorResponse.of(ErrorMessage.INTERNAL_SERVER_ERROR);
+    }
+
 
     @ExceptionHandler(MileException.class)
     public ResponseEntity<ErrorResponse> handleRuntimeException(final MileException e) {
