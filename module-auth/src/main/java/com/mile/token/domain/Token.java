e4d92b96cd9e91263a4123932c90a49261735f93
sohyundoh <lyny.doh@gmail.com>
2024-01-05 17:20:27 +0900

    #13 [feat] 토큰 생성 로직 구현

---
+++ module-auth/src/main/java/com/mile/token/domain/Token.java
@@ -0,0 +1,28 @@
+package com.mile.token.domain;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.redis.core.RedisHash;
+@RedisHash(value = "refreshToken", timeToLive = 60 * 60 * 24 * 1000L * 14)
+@AllArgsConstructor
+@Getter
+@Builder
+public class Token {
+
+    @Id
+    private Long id;
+
+    private String refreshToken;
+
+    public static Token of(
+            final Long id,
+            final String refreshToken
+    ) {
+        return Token.builder()
+                .id(id)
+                .refreshToken(refreshToken)
+                .build();
+    }
+}
