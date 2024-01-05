cca334a8090c1dd5c1f28c7898a5b23c1be8ce9b
sohyundoh <lyny.doh@gmail.com>
2024-01-05 22:35:38 +0900

    #15 [feat] 요청 객체 Wrapping 객체 필터 등록

---
+++ module-common/src/main/java/com/mile/filter/CustomServletWrappingFilter.java
@@ -0,0 +1,27 @@
+package com.mile.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import java.io.IOException;
+
+@Component
+public class CustomServletWrappingFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(final HttpServletRequest request,
+                                    final HttpServletResponse response,
+                                    final FilterChain chain) throws ServletException, IOException {
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+
+        chain.doFilter(requestWrapper, responseWrapper);
+
+        responseWrapper.copyBodyToResponse();
+    }
+}
