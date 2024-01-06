416baaa5ca19bdc692118c67c67390552ecb3d16
sohyundoh <lyny.doh@gmail.com>
2024-01-06 23:20:51 +0900

    #18 [feat] 글감 list 조회 API 엔드포인트 구현

---
+++ module-api/src/main/java/com/mile/controller/moim/MoimControllerSwagger.java
@@ -0,0 +1,33 @@
+package com.mile.controller.moim;
+
+import com.mile.dto.ErrorResponse;
+import com.mile.dto.SuccessResponse;
+import com.mile.moim.serivce.dto.ContentListResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.security.Principal;
+
+@Tag(name = "Moim", description = "모임 관련 API")
+public interface MoimControllerSwagger {
+    @Operation(summary = "에디터 상단 글감 조회")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "글감 조회가 완료되었습니다.",
+                            content = @Content(schema = @Schema(implementation = SuccessResponse.class))),
+                    @ApiResponse(responseCode = "403", description = "해당 사용자는 모임에 접근 권한이 없습니다."),
+                    @ApiResponse(responseCode = "404", description = "해당 모임의 주제가 존재하지 않습니다."),
+                    @ApiResponse(responseCode = "500", description = "서버 내부 오류입니다.",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+            }
+    )
+    SuccessResponse<ContentListResponse> getTopicsFromMoim(
+            @PathVariable final Long moimId,
+            final Principal principal
+    );
+}
