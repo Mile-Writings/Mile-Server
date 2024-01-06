cca005c43f80b0ba2f2b369e8d1dc308fa82a22b
sohyundoh <lyny.doh@gmail.com>
2024-01-06 23:24:15 +0900

    #18 [feat] 글감 List 반환 API 로직 구현

--- module-domain/src/main/java/com/mile/moim/serivce/MoimService.java
+++ module-domain/src/main/java/com/mile/moim/serivce/MoimService.java
@@ -1,7 +1,33 @@
 package com.mile.moim.serivce;
 
+import com.mile.exception.message.ErrorMessage;
+import com.mile.exception.model.UnauthorizedException;
+import com.mile.moim.serivce.dto.ContentListResponse;
+import com.mile.topic.serivce.TopicService;
+import com.mile.writerName.serivce.WriterNameService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MoimService {
+    private final WriterNameService penNameService;
+    private final TopicService topicService;
+
+    public ContentListResponse getContentsFromMoim(
+            final Long moimId,
+            final Long userId
+    ) {
+        authenticateUserOfMoim(moimId, userId);
+        return ContentListResponse.of(topicService.getContentsFromMoim(moimId));
+    }
+
+    private void authenticateUserOfMoim(
+            final Long moimId,
+            final Long userId
+    ) {
+        if (!penNameService.isUserInMoim(moimId, userId)) {
+            throw new UnauthorizedException(ErrorMessage.USER_AUTHENTICATE_ERROR);
+        }
+    }
 }
--- module-domain/src/main/java/com/mile/topic/serivce/TopicService.java
+++ module-domain/src/main/java/com/mile/topic/serivce/TopicService.java
@@ -1,7 +1,54 @@
 package com.mile.topic.serivce;
 
+import com.mile.config.BaseTimeEntity;
+import com.mile.exception.message.ErrorMessage;
+import com.mile.exception.model.NotFoundException;
+import com.mile.topic.domain.Topic;
+import com.mile.topic.repository.TopicRepository;
+import com.mile.topic.serivce.dto.ContentResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class TopicService {
+
+    private final TopicRepository topicRepository;
+
+    public List<ContentResponse> getContentsFromMoim(
+            final Long moimId
+    ) {
+        List<Topic> topicList = sortByCreatedAt(findByMoimId(moimId));
+        isContentsEmpty(topicList);
+        return topicList
+                .stream()
+                .map(ContentResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    private void isContentsEmpty(
+            final List<Topic> topicList
+    ) {
+        if (topicList.isEmpty()) {
+            throw new NotFoundException(ErrorMessage.CONTENT_NOT_FOUND);
+        }
+    }
+
+    private List<Topic> findByMoimId(
+            final Long moimId
+    ) {
+        return topicRepository.findByMoimId(moimId);
+    }
+
+    private List<Topic> sortByCreatedAt(
+            final List<Topic> topicList
+    ) {
+        return topicList.stream()
+                .sorted(Comparator.comparing(BaseTimeEntity::getCreatedAt))
+                .collect(Collectors.toList());
+    }
 }
