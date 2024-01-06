10eea56177c4e34cc49f9c7efc3f9804b60d31b5
sohyundoh <lyny.doh@gmail.com>
2024-01-06 09:48:03 +0900

    #15 [feat] security configuration White List 수정

--- module-common/src/main/java/com/mile/config/security/SecurityConfig.java
+++ module-common/src/main/java/com/mile/config/security/SecurityConfig.java
@@ -24,7 +24,6 @@ public class SecurityConfig {
 
 
     private static final String[] AUTH_WHITELIST = {
-            "/test",
             "/user/login",
             "/user/token-refresh",
             "/actuator/health",
