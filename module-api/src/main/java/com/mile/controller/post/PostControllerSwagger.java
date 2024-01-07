6b817ba7eb5365df10bdee8511ae1ce19368869d
sohyundoh <lyny.doh@gmail.com>
2024-01-07 21:05:39 +0900

    #25 [feat] swagger 수정

--- module-api/src/main/java/com/mile/controller/post/PostControllerSwagger.java
+++ module-api/src/main/java/com/mile/controller/post/PostControllerSwagger.java
@@ -43,8 +43,10 @@ public interface PostControllerSwagger {
     @ApiResponses(
             value = {
                     @ApiResponse(responseCode = "200", description = "댓글 조회가 완료되었습니다."),
-                    @ApiResponse(responseCode = "403", description = "해당 사용자는 모임에 접근 권한이 없습니다."),
-                    @ApiResponse(responseCode = "404", description = "해당 글에 대한 댓글이 존재하지 않습니다."),
+                    @ApiResponse(responseCode = "403", description = "해당 사용자는 모임에 접근 권한이 없습니다.",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "해당 글에 대한 댓글이 존재하지 않습니다.",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
                     @ApiResponse(responseCode = "500", description = "서버 내부 오류입니다.",
                             content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
             }
