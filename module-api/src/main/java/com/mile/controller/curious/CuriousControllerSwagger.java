86e011c76fac277f48f3352682325f01640d8f41
parkheeddong <phj7214go@gmail.com>
2024-01-07 18:12:26 +0900

    #23 [feat] Controller 및 스웨거 인터페이스 생성

--- module-api/src/main/java/com/mile/controller/curious/CuriousControllerSwagger.java
+++ module-api/src/main/java/com/mile/controller/curious/CuriousControllerSwagger.java
@@ -1,2 +1,7 @@
-package com.mile.controller.curious;public class CuriousControllerSwagger {
+package com.mile.controller.curious;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Curious", description = "궁금해요 관련 API")
+public interface CuriousControllerSwagger {
 }
