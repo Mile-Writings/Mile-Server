b62759e30a5931ebcafb293aa8625ada880ce54e
sohyundoh <lyny.doh@gmail.com>
2024-01-07 15:43:41 +0900

    #22 [feat] 댓글 삭제 로직 구현

---
+++ module-domain/src/main/java/com/mile/comment/repository/CommentRepository.java
@@ -0,0 +1,12 @@
+package com.mile.comment.repository;
+
+import com.mile.comment.domain.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query("select c.user.id from Comment c where c = :comment")
+    Long findUserIdByComment(@Param(value = "comment") final Comment comment);
+}
--- module-domain/src/main/java/com/mile/comment/service/CommentService.java
+++ module-domain/src/main/java/com/mile/comment/service/CommentService.java
@@ -1,7 +1,49 @@
 package com.mile.comment.service;
 
+import com.mile.comment.domain.Comment;
+import com.mile.comment.repository.CommentRepository;
+import com.mile.exception.message.ErrorMessage;
+import com.mile.exception.model.ForbiddenException;
+import com.mile.exception.model.NotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    @Transactional
+    public void deleteComment(
+            final Long commentId,
+            final Long userId
+    ) {
+        Comment comment = findById(commentId);
+        authenticateUser(comment, userId);
+        delete(comment);
+    }
+
+    private void delete(
+            final Comment comment
+    ) {
+        commentRepository.delete(comment);
+    }
+    private Comment findById(
+            final Long commentId
+    ) {
+        return commentRepository.findById(commentId)
+                .orElseThrow(
+                        () -> new NotFoundException(ErrorMessage.COMMENT_NOT_FOUND)
+                );
+    }
+    private void authenticateUser(
+            final Comment comment,
+            final Long userId
+    ) {
+        if(!commentRepository.findUserIdByComment(comment).equals(userId))  {
+            throw new ForbiddenException(ErrorMessage.COMMENT_ACCESS_ERROR);
+        }
+    }
 }
