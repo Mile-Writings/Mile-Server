7a8e6f7a67d419d7e21c603e321767c4f04b33e3
parkheeddong <phj7214go@gmail.com>
2024-01-10 00:55:41 +0900

    #39 [feat] 조회 성공 메시지 추가

--- module-common/src/main/java/com/mile/exception/message/SuccessMessage.java
+++ module-common/src/main/java/com/mile/exception/message/SuccessMessage.java
@@ -17,6 +17,7 @@ public enum SuccessMessage {
     MOIM_AUTHENTICATE_SUCCESS(HttpStatus.OK.value(), "사용자의 권한이 확인되었습니다."),
     CURIOUS_INFO_SEARCH_SUCCESS(HttpStatus.OK.value(), "궁금해요 개수 및 궁금해요 여부 조회가 완료되었습니다."),
     CURIOUS_DELETE_SUCCESS(HttpStatus.OK.value(), "궁금해요 삭제가 완료되었습니다."),
+    MOIM_POPULAR_WRITER_SEARCH_SUCCESS(HttpStatus.OK.value(), "궁금해요 TOP 2 작가 조회가 완료되었습니다."),
     /*
     201 CREATED
      */
