5affda65a7d192c688f6f65a3480b0c6f5431987
sohyundoh <lyny.doh@gmail.com>
2024-01-10 01:26:55 +0900

    #45 [feat] 소셜 로그인 Refresh Token Http-Only 쿠키 반환

--- module-api/src/main/java/com/mile/controller/user/UserController.java
+++ module-api/src/main/java/com/mile/controller/user/UserController.java
@@ -7,7 +7,11 @@ import com.mile.token.service.TokenService;
 import com.mile.user.service.UserService;
 import com.mile.user.service.dto.AccessTokenGetSuccess;
 import com.mile.user.service.dto.LoginSuccessResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,17 +29,29 @@ public class UserController implements UserControllerSwagger {
 
     private final UserService userService;
     private final TokenService tokenService;
+    private final static int COOKIE_MAX_AGE = 7 * 24 * 60 * 60;
+    private final static String REFRESH_TOKEN = "refreshToken";
 
     @PostMapping("/login")
     @Override
-    public SuccessResponse<LoginSuccessResponse> login(
+    public ResponseEntity<SuccessResponse<AccessTokenGetSuccess>> login(
             @RequestParam final String authorizationCode,
-            @RequestBody final UserLoginRequest loginRequest
+            @RequestBody final UserLoginRequest loginRequest,
+            HttpServletResponse response
     ) {
-        return SuccessResponse.of(SuccessMessage.LOGIN_SUCCESS, userService.create(authorizationCode, loginRequest));
+        LoginSuccessResponse successResponse = userService.create(authorizationCode, loginRequest);
+        ResponseCookie cookie = ResponseCookie.from(REFRESH_TOKEN, successResponse.refreshToken())
+                .maxAge(COOKIE_MAX_AGE)
+                .path("/")
+                .secure(true)
+                .sameSite("None")
+                .httpOnly(true)
+                .build();
+        response.setHeader("Set-Cookie", cookie.toString());
+        return ResponseEntity.status(HttpStatus.OK).body(SuccessResponse.of(SuccessMessage.LOGIN_SUCCESS, AccessTokenGetSuccess.of(successResponse.accessToken())));
     }
 
-    @GetMapping("/token-refresh")
+    @GetMapping("/token/reissue")
     @Override
     public SuccessResponse<AccessTokenGetSuccess> refreshToken(
             @RequestParam final String refreshToken
@@ -43,7 +59,7 @@ public class UserController implements UserControllerSwagger {
         return SuccessResponse.of(SuccessMessage.ISSUE_ACCESS_TOKEN_SUCCESS, userService.refreshToken(refreshToken));
     }
 
-    @DeleteMapping("/delete")
+    @DeleteMapping(" /delete")
     @Override
     public SuccessResponse deleteUser(
             final Principal principal
