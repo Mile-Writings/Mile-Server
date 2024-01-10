13c73f363341d472e8045d9bf373b54872b8e15b
sohyundoh <lyny.doh@gmail.com>
2024-01-11 00:15:53 +0900

    #57 [feat] redis 관련 모듈 제거

--- module-api/src/main/java/com/mile/controller/user/UserController.java
+++ module-api/src/main/java/com/mile/controller/user/UserController.java
@@ -3,13 +3,10 @@ package com.mile.controller.user;
 import com.mile.dto.SuccessResponse;
 import com.mile.exception.message.SuccessMessage;
 import com.mile.external.client.dto.UserLoginRequest;
-import com.mile.token.service.TokenService;
 import com.mile.user.service.UserService;
-import com.mile.user.service.dto.AccessTokenGetSuccess;
 import com.mile.user.service.dto.LoginSuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,8 +21,6 @@ import java.security.Principal;
 public class UserController implements UserControllerSwagger {
 
     private final UserService userService;
-    private final TokenService tokenService;
-
     @PostMapping("/login")
     @Override
     public SuccessResponse<LoginSuccessResponse> login(
@@ -35,13 +30,6 @@ public class UserController implements UserControllerSwagger {
         return SuccessResponse.of(SuccessMessage.LOGIN_SUCCESS, userService.create(authorizationCode, loginRequest));
     }
 
-    @GetMapping("/token-refresh")
-    @Override
-    public SuccessResponse<AccessTokenGetSuccess> refreshToken(
-            @RequestParam final String refreshToken
-    ) {
-        return SuccessResponse.of(SuccessMessage.ISSUE_ACCESS_TOKEN_SUCCESS, userService.refreshToken(refreshToken));
-    }
 
     @DeleteMapping("/delete")
     @Override
@@ -52,13 +40,6 @@ public class UserController implements UserControllerSwagger {
         return SuccessResponse.of(SuccessMessage.USER_DELETE_SUCCESS);
     }
 
-    @PostMapping("/logout")
-    @Override
-    public SuccessResponse logout(
-            final Principal principal
-    ) {
-        tokenService.deleteRefreshToken(Long.valueOf(principal.getName()));
-        return SuccessResponse.of(SuccessMessage.LOGOUT_SUCCESS);
-    }
+
 }
 
