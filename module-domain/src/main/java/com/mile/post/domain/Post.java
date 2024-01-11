1892b7aa83040b56ef286903e980dc9fdc0f7908
parkheeddong <phj7214go@gmail.com>
2024-01-12 02:44:36 +0900

    #17 [feat] Post create 메서드 생성

--- module-domain/src/main/java/com/mile/post/domain/Post.java
+++ module-domain/src/main/java/com/mile/post/domain/Post.java
@@ -10,10 +10,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
 public class Post extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,6 +46,30 @@ public class Post extends BaseTimeEntity {
         this.curiousCount--;
     }
 
+    public static Post create(
+            final Topic topic,
+            final WriterName writerName,
+            final String title,
+            final String content,
+            final String imageUrl,
+            final boolean anonymous,
+            final boolean isTemporary
+    ) {
+        final boolean containPhoto = imageUrl != null && !imageUrl.isEmpty();
+        return Post
+                .builder()
+                .topic(topic)
+                .writerName(writerName)
+                .title(title)
+                .content(content)
+                .imageUrl(imageUrl)
+                .curiousCount(0)
+                .containPhoto(containPhoto)
+                .anonymous(anonymous)
+                .isTemporary(isTemporary)
+                .build();
+    }
+
     public void updatePost(
             final Topic topic,
             final PostPutRequest putRequest
@@ -49,4 +80,5 @@ public class Post extends BaseTimeEntity {
         this.imageUrl = putRequest.imageUrl();
         this.anonymous = putRequest.anonymous();
     }
+
 }
