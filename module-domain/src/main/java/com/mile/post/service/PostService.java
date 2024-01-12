2a2a3a74cd6103e308a65838b96376664672e797
parkheeddong <phj7214go@gmail.com>
2024-01-12 18:43:44 +0900

    #80 [refactor] 임시저장이 아닌 글이므로 수정

--- module-domain/src/main/java/com/mile/post/service/PostService.java
+++ module-domain/src/main/java/com/mile/post/service/PostService.java
@@ -209,7 +209,7 @@ public class PostService {
                 postCreateRequest.imageUrl(),
                 checkContainPhoto(postCreateRequest.imageUrl()),
                 postCreateRequest.anonymous(),
-                isTemporaryPost
+                !isTemporaryPost
         ));
     }
 
