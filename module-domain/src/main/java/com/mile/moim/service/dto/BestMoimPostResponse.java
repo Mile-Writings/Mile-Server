b2e53951a3c20ba98aab360833e3ad9376862ec7
parkheeddong <phj7214go@gmail.com>
2024-01-13 02:42:32 +0900

    #84 [feat] html 태그 제거

--- module-domain/src/main/java/com/mile/moim/service/dto/BestMoimPostResponse.java
+++ module-domain/src/main/java/com/mile/moim/service/dto/BestMoimPostResponse.java
@@ -1,14 +1,32 @@
 package com.mile.moim.service.dto;
 
 import com.mile.post.domain.Post;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
 
 public record BestMoimPostResponse(String topicName, String imageUrl, String postTitle, String postContent) {
+    private static final int SUBSTRING_START = 0;
+    private static final int SUBSTRING_END = 200;
+
     public static BestMoimPostResponse of(Post post) {
+
         return new BestMoimPostResponse(
                 post.getTopic().getContent(),
                 post.getImageUrl(),
                 post.getTitle(),
-                post.getContent()
+                getSubStringOfCleanContent(post.getContent())
         );
     }
+
+    private static String getSubStringOfCleanContent(
+            String content
+    ) {
+        return Jsoup.clean(content, Whitelist.none()).substring(SUBSTRING_START, SUBSTRING_END);
+    }
 }
+
+/*
+    private static String getSubString(final Post post) {
+        return Jsoup.clean(post.getContent(), Whitelist.none()).substring(SUBSTRING_START, SUBSTRING_END);
+ */
+
