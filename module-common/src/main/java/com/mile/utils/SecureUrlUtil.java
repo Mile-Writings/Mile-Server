da50cf23a2df92923622427477eb7c9dab52f50e
sohyundoh <lyny.doh@gmail.com>
2024-01-17 21:24:55 +0900

    #161 [feat] url 디코딩 예외 처리

--- module-common/src/main/java/com/mile/utils/SecureUrlUtil.java
+++ module-common/src/main/java/com/mile/utils/SecureUrlUtil.java
@@ -1,5 +1,7 @@
 package com.mile.utils;
 
+import com.mile.exception.message.ErrorMessage;
+import com.mile.exception.model.BadRequestException;
 import org.springframework.stereotype.Component;
 
 import java.util.Base64;
@@ -12,7 +14,14 @@ public class SecureUrlUtil {
     }
 
     public Long decodeUrl(final String url) {
+        checkURLValue(url);
         return Long.parseLong(new String(Base64.getUrlDecoder().decode(url)));
+    }
+
+    private void checkURLValue(final String url) {
+        if(url.length() < 4) {
+            throw new BadRequestException(ErrorMessage.PATH_PARAMETER_INVALID_ERROR);
+        }
     }
 
 }
