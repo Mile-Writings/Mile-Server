5659e9ede73e3e88a8dd78b4a2a0b10f43616695
sohyundoh <lyny.doh@gmail.com>
2024-01-18 18:38:30 +0900

    #107 [feat] 모델 작성

---
+++ module-common/src/main/java/com/mile/log/discord/model/Thumbnail.java
@@ -0,0 +1,11 @@
+package com.mile.log.discord.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class Thumbnail {
+
+    private final String url;
+}
