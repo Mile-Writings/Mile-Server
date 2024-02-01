982abbc201dc4ed175376e4e9ce512353762f3d4
parkheeddong <phj7214go@gmail.com>
2024-02-01 17:30:16 +0900

    #217 [refactor] 네이밍 변경

--- module-domain/src/main/java/com/mile/moim/repository/MoimRepositoryCustomImpl.java
+++ module-domain/src/main/java/com/mile/moim/repository/MoimRepositoryCustomImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class MoimRepositoryImpl implements MoimRepositoryCustom {
+public class MoimRepositoryCustomImpl implements MoimRepositoryCustom {
     private final JPAQueryFactory queryFactory;
 
     public List<Moim> findTop3MoimsByPostCountInLastWeek() {
