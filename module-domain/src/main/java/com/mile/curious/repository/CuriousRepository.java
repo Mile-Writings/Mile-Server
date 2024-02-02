147b4bc3d0eab347183716cda1aec978c4768980
sohyundoh <lyny.doh@gmail.com>
2024-02-02 22:04:22 +0900

    #211 [refactor] batch delete 방식으로 변경

--- module-domain/src/main/java/com/mile/curious/repository/CuriousRepository.java
+++ module-domain/src/main/java/com/mile/curious/repository/CuriousRepository.java
@@ -3,10 +3,15 @@ import com.mile.curious.domain.Curious;
 import com.mile.post.domain.Post;
 import com.mile.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CuriousRepository extends JpaRepository<Curious, Long> {
     boolean existsByPostAndUser(Post post, User user);
     Curious findByPostAndUser(Post post, User user);
 
-    void deleteAllByPost(final Post post);
+    @Modifying
+    @Query("delete from Curious c where c.post = :post")
+    void deleteAllByPost(@Param("post") final Post post);
 }
