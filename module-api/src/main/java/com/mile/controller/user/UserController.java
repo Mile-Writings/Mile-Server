5fe10dbcd5d5c8a1bac95b4630df1990fcde52ca
sohyundoh <lyny.doh@gmail.com>
2024-02-11 20:32:19 +0900

    #225 [feat] accessToken 재발급 API 작성

--- module-api/src/main/java/com/mile/controller/user/UserController.java
+++ module-api/src/main/java/com/mile/controller/user/UserController.java
@@ -1,8 +1,10 @@
 package com.mile.controller.user;
 
+import com.mile.authentication.PrincipalHandler;
 import com.mile.dto.SuccessResponse;
 import com.mile.exception.message.SuccessMessage;
 import com.mile.external.client.dto.UserLoginRequest;
+import com.mile.jwt.redis.service.TokenService;
 import com.mile.user.service.UserService;
 import com.mile.user.service.dto.AccessTokenGetSuccess;
 import com.mile.user.service.dto.LoginSuccessResponse;
@@ -12,13 +14,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/user")
@@ -26,6 +27,8 @@ import java.security.Principal;
 public class UserController implements UserControllerSwagger {
 
     private final UserService userService;
+    private final TokenService tokenService;
+    private final PrincipalHandler principalHandler;
     private final static int COOKIE_MAX_AGE = 7 * 24 * 60 * 60;
     private final static String REFRESH_TOKEN = "refreshToken";
 
@@ -49,12 +52,28 @@ public class UserController implements UserControllerSwagger {
     }
 
 
+    @GetMapping("/refresh-token")
+    @Override
+    public SuccessResponse<AccessTokenGetSuccess> refreshToken(
+            @RequestParam final String refreshToken
+    ) {
+        return SuccessResponse.of(SuccessMessage.ISSUE_ACCESS_TOKEN_SUCCESS, userService.refreshToken(refreshToken));
+    }
+
+
+    @PostMapping("/logout")
+    @Override
+    public SuccessResponse logout(
+    ) {
+        tokenService.deleteRefreshToken(principalHandler.getUserIdFromPrincipal());
+        return SuccessResponse.of(SuccessMessage.LOGOUT_SUCCESS);
+    }
+
     @DeleteMapping("/delete")
     @Override
     public SuccessResponse deleteUser(
-            final Principal principal
     ) {
-        userService.deleteUser(Long.valueOf(principal.getName()));
+        userService.deleteUser(principalHandler.getUserIdFromPrincipal());
         return SuccessResponse.of(SuccessMessage.USER_DELETE_SUCCESS);
     }
 
