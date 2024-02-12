a79d8416ec826cc35c84844aba8afc1841ddfdb4
sohyundoh <lyny.doh@gmail.com>
2024-02-12 17:21:54 +0900

    #232 [feat] 연관관계 fetchType 변경

--- module-domain/src/main/java/com/mile/moim/domain/Moim.java
+++ module-domain/src/main/java/com/mile/moim/domain/Moim.java
@@ -4,6 +4,7 @@ import com.mile.config.BaseTimeEntity;
 import com.mile.writername.domain.WriterName;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -16,7 +17,7 @@ public class Moim extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private WriterName owner;
     private String name;
     private String imageUrl;
