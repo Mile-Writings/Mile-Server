d9b81a8357f90a4acf8b76ea5f10c573095e05c5
sohyundoh <lyny.doh@gmail.com>
2024-03-24 13:03:23 +0900

    #249 [feat] comment count 메서드 작성

--- module-domain/src/main/java/com/mile/comment/service/CommentService.java
+++ module-domain/src/main/java/com/mile/comment/service/CommentService.java
@@ -109,6 +109,12 @@ public class CommentService {
         return commentRepository.findByPostId(postId);
     }
 
+    public int findCommentCountByPost(
+            final Post post
+    ) {
+        return findByPostId(post.getId()).size();
+    }
+
 
     private boolean isCommentListNull(
             final List<Comment> commentList
