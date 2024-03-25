c3681a2fe0df4bd5a47a93b5b15a9d845c129250
parkheeddong <phj7214go@gmail.com>
2024-03-26 00:04:45 +0900

    #258 [feat] 중복확인 서비스로직 구현

--- module-domain/src/main/java/com/mile/moim/service/MoimService.java
+++ module-domain/src/main/java/com/mile/moim/service/MoimService.java
@@ -10,6 +10,7 @@ import com.mile.moim.service.dto.ContentListResponse;
 import com.mile.moim.service.dto.MoimAuthenticateResponse;
 import com.mile.moim.service.dto.MoimCuriousPostListResponse;
 import com.mile.moim.service.dto.MoimInfoResponse;
+import com.mile.moim.service.dto.MoimNameConflictCheckResponse;
 import com.mile.moim.service.dto.MoimTopicResponse;
 import com.mile.moim.service.dto.TemporaryPostExistResponse;
 import com.mile.moim.service.dto.TopicListResponse;
@@ -157,5 +158,11 @@ public class MoimService {
     ) {
         String postId = postCreateService.getTemporaryPostExist(findById(moimId), writerNameService.findByWriterId(userId));
         return TemporaryPostExistResponse.of(!secureUrlUtil.decodeUrl(postId).equals(0L), postId);
+    }
+
+    public MoimNameConflictCheckResponse validateMoimName(
+            final String moimName
+    ) {
+        return MoimNameConflictCheckResponse.of(!moimRepository.existsByName(moimName));
     }
 }
