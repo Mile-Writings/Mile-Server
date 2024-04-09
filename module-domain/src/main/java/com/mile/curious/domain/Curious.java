e926dc7ab858a88d073e2849950ef477336ed6e3
parkheeddong <phj7214go@gmail.com>
2024-04-09 22:03:47 +0900

    #284 [feat] 삭제될 멤버의 궁금해요 삭제 로직

--- module-domain/src/main/java/com/mile/curious/domain/Curious.java
+++ module-domain/src/main/java/com/mile/curious/domain/Curious.java
@@ -11,10 +11,12 @@ import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Builder
+@Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor
 public class Curious extends BaseTimeEntity {
