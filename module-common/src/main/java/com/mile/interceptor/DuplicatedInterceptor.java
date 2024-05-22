e3a8df6746dde30631c79b591bf7eafbbd6560c1
sohyundoh <lyny.doh@gmail.com>
2024-05-22 22:49:01 +0900

    #340 [feat] Duplicated Interceptor UserId 처리 부분 정리

--- module-common/src/main/java/com/mile/interceptor/DuplicatedInterceptor.java
+++ module-common/src/main/java/com/mile/interceptor/DuplicatedInterceptor.java
@@ -1,9 +1,34 @@
 package com.mile.interceptor;
 
+import com.mile.config.filter.PrincipalHandler;
+import com.mile.filter.wrapper.CachedBodyRequestWrapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
+@RequiredArgsConstructor
 public class DuplicatedInterceptor implements HandlerInterceptor {
 
+    private static final String REDIS_KEY = "MILE_REDIS";
+    private static final String RMAP_VALUE = "MILE";
+    private final RedissonClient redissonClient;
+    private final PrincipalHandler principalHandler;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+
+    }
+
+    private String getRmapKey() {
+        return principalHandler.getUserIdFromPrincipal().toString();
+    }
+
+    private boolean lock(HttpSerletRequest request) {
+        final String rmapKey = getRmapKey()
+    }
 }
+
