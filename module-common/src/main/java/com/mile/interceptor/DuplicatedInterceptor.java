d768d1f1f460628ce13e5ce6d51cc7bb03918d17
sohyundoh <lyny.doh@gmail.com>
2024-05-22 22:26:19 +0900

    #340 [feat] 인터셉트 생성 및 등록

---
+++ module-common/src/main/java/com/mile/interceptor/DuplicatedInterceptor.java
@@ -0,0 +1,9 @@
+package com.mile.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class DuplicatedInterceptor implements HandlerInterceptor {
+
+}
