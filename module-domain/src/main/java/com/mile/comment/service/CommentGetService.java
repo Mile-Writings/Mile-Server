3c893ec6eb9466aecd45d79acfcf4b375304cb8c
sohyundoh <lyny.doh@gmail.com>
2024-06-07 16:38:57 +0900

    #381 [feat] Comment Get Service(helper 계층 도입) 및 Comment Reply 개수 로직 작성

--- module-domain/src/main/java/com/mile/comment/service/CommentGetService.java
+++ module-domain/src/main/java/com/mile/comment/service/CommentGetService.java
@@ -1,18 +1,20 @@
 package com.mile.comment.service;
 
 import com.mile.comment.repository.CommentRepository;
+import com.mile.commentreply.service.CommentReplyService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 @RequiredArgsConstructor
 public class CommentGetService {
 
     private final CommentRepository commentRepository;
+    private final CommentReplyService commentReplyService;
 
     public int findCommentCountByWriterNameId(
             final Long writerNameId
     ) {
-        return commentRepository.countByWriterNameId(writerNameId);
+        return commentRepository.countByWriterNameId(writerNameId) + commentReplyService.countByWriterNameId(writerNameId);
     }
 }
--- module-domain/src/main/java/com/mile/commentreply/repository/CommentReplyRepository.java
+++ module-domain/src/main/java/com/mile/commentreply/repository/CommentReplyRepository.java
@@ -2,6 +2,7 @@ package com.mile.commentreply.repository;
 
 import com.mile.comment.domain.Comment;
 import com.mile.commentreply.domain.CommentReply;
+import com.mile.writername.domain.WriterName;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
 public interface CommentReplyRepository extends JpaRepository<CommentReply, Long> {
 
     List<CommentReply> findByComment(final Comment comment);
+
+    int countByWriterNameId(final Long writerNameId);
 }
