c771707dfc10d21af317fd1c31f74b1826222181
parkheeddong <phj7214go@gmail.com>
2024-06-25 22:24:25 +0900

    #395 [feat] Creator 분리

---
+++ module-domain/src/main/java/com/mile/topic/service/TopicCreator.java
@@ -0,0 +1,30 @@
+package com.mile.topic.service;
+
+import com.mile.moim.domain.Moim;
+import com.mile.moim.service.dto.TopicCreateRequest;
+import com.mile.topic.domain.Topic;
+import com.mile.topic.repository.TopicRepository;
+import com.mile.utils.SecureUrlUtil;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class TopicCreator {
+
+    private final TopicRepository topicRepository;
+    private final SecureUrlUtil secureUrlUtil;
+
+    @Transactional
+    public Long createTopicOfMoim(
+            final Moim moim,
+            final TopicCreateRequest createRequest
+    ) {
+        Topic topic = topicRepository.saveAndFlush(Topic.create(moim, createRequest));
+        topic.setIdUrl(secureUrlUtil.encodeUrl(topic.getId()));
+        return topic.getId();
+    }
+}
