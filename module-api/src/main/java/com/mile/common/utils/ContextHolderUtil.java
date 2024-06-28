8013cb338ec59360e88c7377aa455224869052a8
sohyundoh <lyny.doh@gmail.com>
2024-06-28 20:20:39 +0900

    #412 [fix] duplicate interceptor 토큰 인증 제외

--- module-api/src/main/java/com/mile/common/utils/ContextHolderUtil.java
+++ module-api/src/main/java/com/mile/common/utils/ContextHolderUtil.java
@@ -4,9 +4,12 @@ import com.mile.common.auth.JwtTokenProvider;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
@@ -15,7 +18,11 @@ public class ContextHolderUtil {
     private final JwtTokenProvider jwtTokenProvider;
 
     public String getUserIdFromContextHolder() {
-        HttpServletRequest servletRequest = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        HttpServletRequest servletRequest = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
+
+        if (HttpMethod.OPTIONS.matches(servletRequest.getMethod())) {
+            return null;
+        }
         return jwtTokenProvider.getUserFromJwt(servletRequest.getHeader("Authorization")).toString();
     }
 
