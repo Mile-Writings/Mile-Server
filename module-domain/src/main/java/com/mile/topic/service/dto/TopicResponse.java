d37e6b0e0a484c2dfca56af1515df36a24cca7e2
sohyundoh <lyny.doh@gmail.com>
2024-08-06 13:21:42 +0900

    #448 [feat] 필드 값 수정

--- module-domain/src/main/java/com/mile/topic/service/dto/TopicResponse.java
+++ module-domain/src/main/java/com/mile/topic/service/dto/TopicResponse.java
@@ -2,7 +2,7 @@ package com.mile.topic.service.dto;
 
 import com.mile.topic.domain.Topic;
 
-public record TopicResponse(Long id, String topicName) {
+public record TopicResponse(Long topicId, String topicName) {
     public static TopicResponse of(final Topic topic) {
         return new TopicResponse(topic.getId(), topic.getKeyword());
     }
