c3922114c9eb65527b23fe59ab97244cee71636d
sohyundoh <lyny.doh@gmail.com>
2024-08-17 16:18:38 +0900

    #493 [feat] remove dto blank validation

--- module-domain/src/main/java/com/mile/writername/service/dto/WriterNameDescriptionUpdateRequest.java
+++ module-domain/src/main/java/com/mile/writername/service/dto/WriterNameDescriptionUpdateRequest.java
@@ -1,10 +1,9 @@
 package com.mile.writername.service.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record WriterNameDescriptionUpdateRequest(
-        @NotBlank(message = "소개 글이 입력되지 않았습니다.")
+        
         @Size(max = 110, message = "소개 글은 최대 110자 이내로 작성해주세요.")
         String description
 ) {
