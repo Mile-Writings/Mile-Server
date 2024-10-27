161cd4a30018dc53f57b1ff6c7afe8651776c90e
sohyundoh <lyny.doh@gmail.com>
2024-10-27 20:18:46 +0900

    #547 [feat] n+1 쿼리 문제 해결

--- module-domain/src/main/java/com/mile/moim/repository/MoimPopularInfoRepository.java
+++ module-domain/src/main/java/com/mile/moim/repository/MoimPopularInfoRepository.java
@@ -2,7 +2,6 @@ package com.mile.moim.repository;
 
 import com.mile.moim.domain.popular.MoimPopularInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.Repository;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.Optional;
