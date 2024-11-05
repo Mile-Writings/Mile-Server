74270a93fcf9472a866fa97acaa3b8938c0644cb
sohyundoh <lyny.doh@gmail.com>
2024-11-05 21:46:31 +0900

    #569 [feat] InternalController HTTP 메서드 변경

--- module-api/src/main/java/com/mile/controller/external/InternalController.java
+++ module-api/src/main/java/com/mile/controller/external/InternalController.java
@@ -2,8 +2,7 @@ package com.mile.controller.external;
 
 import com.mile.common.CacheService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +11,7 @@ public class InternalController {
 
     private final CacheService cacheService;
 
-    @DeleteMapping("/api/v1/moim/info/cache")
+    @PostMapping("/api/v1/moim/info/cache")
     public void deleteMoimInfoCache() {
         cacheService.deleteMoimCache();
     }
