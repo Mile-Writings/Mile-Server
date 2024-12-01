9708fb47d2da8b331a5189eeced865aaee8522c5
sohyundoh <lyny.doh@gmail.com>
2024-12-01 19:07:25 +0900

    #585 [feat] 레디스 장애 시 RedisException 캐치 후 로그 남기고 분산락 안걸도록 수정

--- module-domain/src/main/java/com/mile/common/lock/DistributedLock.java
+++ module-domain/src/main/java/com/mile/common/lock/DistributedLock.java
@@ -2,31 +2,47 @@ package com.mile.common.lock;
 
 import com.mile.exception.message.ErrorMessage;
 import com.mile.exception.model.MileException;
+import com.mile.slack.module.SendErrorModule;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
+import org.redisson.client.RedisException;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class DistributedLock {
 
     private final RedissonClient redissonClient;
+    private final SendErrorModule sendErrorModule;
 
     public void getLock(final String key) {
-        final RLock lock = redissonClient.getLock(key);
-        try {
-            checkAvailability(lock.tryLock(3, 4, TimeUnit.SECONDS));
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        try{
+            final RLock lock = redissonClient.getLock(key);
+            try {
+                checkAvailability(lock.tryLock(2, 3, TimeUnit.SECONDS));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (RedisException e) {
+            log.error("레디스 락 획득 중 에러 발생");
+            sendErrorModule.sendError(e);
         }
     }
 
     public void afterLock(final String key) {
-        final RLock lock = redissonClient.getLock(key);
-        lock.unlock();
+
+       try {
+           final RLock lock = redissonClient.getLock(key);
+           lock.unlock();
+       } catch (RedisException e) {
+           log.error("레디스 락 해제 중 에러 발생");
+           sendErrorModule.sendError(e);
+       }
     }
 
     public void checkAvailability(final Boolean available) {
